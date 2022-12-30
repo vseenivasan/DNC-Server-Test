8a27f3fd711d9511946fdc8f03bf0dd728636c9e
Seenivasan <seenivasanv@mcci.com>
2022-12-30 12:28:49 +0530

    version update

--- app/misc/constants.js
+++ app/misc/constants.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
     APP_NAME: 'Generic DNC API Test Server',
-    APP_VERSION: '1.0.0-1',
+    APP_VERSION: '1.0.0-2',
 	KEY_SECRET: 'mcci*123',
     APP_PORT: 8891
 });
